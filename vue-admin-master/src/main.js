fff1eac4a0810a7ea518fb39d4b097b3414aa721
xwg <xwg@qq.com>
2019-02-26 13:27:53 +0800

    Initial commit

---
+++ vue-admin-master/src/main.js
@@ -0,0 +1,57 @@
+import babelpolyfill from 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+//import './assets/theme/theme-green/index.css'
+import VueRouter from 'vue-router'
+import store from './vuex/store'
+import Vuex from 'vuex'
+//import NProgress from 'nprogress'
+//import 'nprogress/nprogress.css'
+import routes from './routes'
+// import Mock from './mock'
+// Mock.bootstrap();
+import 'font-awesome/css/font-awesome.min.css'
+import axios from 'axios'
+
+Vue.use(ElementUI)
+Vue.use(VueRouter)
+Vue.use(Vuex)
+
+axios.defaults.baseURL="https://www.easy-mock.com/mock/5c74c434261e0e3c3307e3cb/employee";
+Vue.prototype.$http = axios;
+Vue.config.productionTip = false;
+
+//NProgress.configure({ showSpinner: false });
+
+const router = new VueRouter({
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  //NProgress.start();
+  if (to.path == '/login') {
+    sessionStorage.removeItem('user');
+  }
+  let user = JSON.parse(sessionStorage.getItem('user'));
+  if (!user && to.path != '/login') {
+    next({ path: '/login' })
+  } else {
+    next()
+  }
+})
+
+//router.afterEach(transition => {
+//NProgress.done();
+//});
+
+new Vue({
+  //el: '#app',
+  //template: '<App/>',
+  router,
+  store,
+  //components: { App }
+  render: h => h(App)
+}).$mount('#app')
+
